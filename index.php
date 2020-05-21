101128b23ec7b6798db399fa687266d7ef055c23
Boujaji <58435835+Boujaji@users.noreply.github.com>
2020-05-21 11:04:35 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+/*****************************
+ * TIC TAC TOE		     *
+ *			     *
+ * Created by 		     *
+ * Ayoub Djenidi 	     *
+ * Theo Le Lez  	     *
+ * Loup Federico	     *
+ *****************************
+ */
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Tic Tac Toe!</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link href='http://fonts.googleapis.com/css?family=Permanent+Marker' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<section>
+<div class="main">
+    <h1>Tic Tac Toe</h1>
+<form action="game.php" method="post">
+    <p>Please pick your piece:</p>
+
+
+    <p class="x">X
+    <input type="radio" name="piece" value="X" checked>
+    </p>
+    <p class="o">O
+    <input type="radio" name="piece" value="O">
+    </p>
+    <input type="hidden" name="turnNum" value="1">
+    <button type="submit" name="submit" value="Submit">Submit</button>
+</form>
+
+    </div>
+
+</section>
+
+</body>
+</html>
